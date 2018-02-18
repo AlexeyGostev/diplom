85884832b7b2d609f40a5fb46c675b364cc972cf
Alexey Gostev <AlGost96@mail.ru>
2018-02-19 00:14:08 +0300

    created models of users

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const crypto = require('crypto');
+
+const mongoose = require('../libs/mongoose');
+const Schema = mongoose.Schema;
+
+let schema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  patronymic: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default : ''
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  }
+}, { usePushEach: true });
+
+schema.virtual('password')
+  .set(function(password) {
+    this._plainPassword = password;
+    this.salt = crypto.randomBytes(32).toString('base64');
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._plainPassword;
+  });
+
+schema.methods.encryptPassword = function(password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+};
+
+schema.methods.checkPassword = function(password) {
+  return this.encryptPassword(password) === this.hashedPassword;
+};
+
+module.exports = mongoose.model('User', schema);
+
