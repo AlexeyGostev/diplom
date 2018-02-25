e22d2591f37facc6c94ade3e794001488c73a5dd
Alexey Gostev <AlGost96@mail.ru>
2018-02-25 18:19:35 +0300

    created check-password method in repository

---
+++ repositories/users/check-password.js
@@ -0,0 +1,17 @@
+const User = require('../../models/user');
+const log = require('../../logger')(module);
+
+
+module.exports = async (login, password) => {
+  try {
+    let user = await User.findOne({login: login});
+    if (user && user.checkPassword(password)) {
+      return user;
+    } else {
+      return false;
+    }
+  } catch(err) {
+    log.error(err);
+    return false;
+  }
+};
